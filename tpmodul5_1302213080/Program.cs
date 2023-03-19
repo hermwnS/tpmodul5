8b305fd2d8ce988ac848bffd404f732611bc7a63
hermwnS <126874286+hermwnS@users.noreply.github.com>
2023-03-19 21:24:07 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_1302213080/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T X)
+        {
+            Console.WriteLine("Halo user : " + X);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        HaloGeneric.SapaUser<string>("Hermawan");
+    }
+}
